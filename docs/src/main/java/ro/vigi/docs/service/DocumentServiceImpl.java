4b3dee03315dd011e588f1ccce8f88997edb64db
vigi <vigi@192.168.0.191>
2012-11-13 20:31:02 +0100

    - added first rest web service

--- docs/src/main/java/ro/vigi/docs/service/DocumentServiceImpl.java
+++ docs/src/main/java/ro/vigi/docs/service/DocumentServiceImpl.java
@@ -1,5 +1,14 @@
 package ro.vigi.docs.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import ro.vigi.docs.domain.Document;
 
 public class DocumentServiceImpl implements DocumentService {
+
+	@Override
+	public Page<Document> findAllDocuments(final Pageable pageable) {
+		return documentRepository.findAll(pageable);
+	}
 }
